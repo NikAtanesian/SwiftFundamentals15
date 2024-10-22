ca1ee37dd38e85e7a7dbbadcb862c302f55dd330
Nikolas Atanesian <atanesiannik@gmail.com>
2024-10-22 11:34:56 -0400

    Update Contents.swift

--- student_Xcode15/Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,13 +10,16 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("In the Air Tonight (song title) by Phil Collins (artist)")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
+print("Well if you told me you were drowning, I would not lend a hand.")
+print("I've seen your face before my friend, but I don't know if you know who I am.")
+print("And I was there and I saw what you did, I saw it with my own too eyes.")
+print("So you can wipe off that grin, I know where you've been.")
+print("It's all been a pack of lies.")
 
 /*:
 
