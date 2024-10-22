21b3dbb5e7fee1c59857125957559af635bf0fd8
Nikolas Atanesian <atanesiannik@gmail.com>
2024-10-22 17:51:55 -0400

    Update Contents.swift

--- student_Xcode15/Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/6. Exercise - Switch Statements.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/6. Exercise - Switch Statements.xcplaygroundpage/Contents.swift
@@ -6,7 +6,17 @@
 
 
 //:  Write a new `switch` statement that prints "Medal winner" if `leaguePosition` is within the range of 1-3. Otherwise, print "No medal awarded."
-
+let leaguePosition = 1
+switch leaguePosition {
+case 1:
+ print("Champions!")
+case 2:
+ print("Runners up")
+case 3:
+ print("Third place")
+default:
+ print("Bad season!")
+}
 
 /*:
 [Previous](@previous)  |  page 6 of 9  |  [Next: App Exercise - Heart Rate Zones](@next)
