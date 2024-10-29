0e8faf0b5863515603197949b332a3341d872727
Nikolas Atanesian <atanesiannik@gmail.com>
2024-10-29 15:45:23 -0400

    Created InterfaceBuildingBasics Shell

---
+++ student_Xcode15/Unit1/Lesson1_8/InterfaceBuildingBasics/InterfaceBuildingBasics/InterfaceBuildingBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceBuildingBasicsApp.swift
+//  InterfaceBuildingBasics
+//
+//  Created by RemoteStudent on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceBuildingBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
