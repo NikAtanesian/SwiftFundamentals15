a123d086928fc5d9b2327aefa672694cf7216916
Nikolas Atanesian <atanesiannik@gmail.com>
2024-11-12 11:58:50 -0500

    Update Contents.swift

--- student_Xcode15/Unit2/Lesson2_2/Lab_Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_2/Lab_Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -20,11 +20,11 @@ print(favoriteQuote)
 
 //:  Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
 let emptyString = ""
-if (emptyString = "")
+if (emptyString == ""){
   print("There's nothing here")
-else
+} else {
   print("It's not as empty as I thought")
-
+}
 /*:
 page 1 of 5  |  [Next: Exercise - Concatenation and Interpolation](@next)
  */
